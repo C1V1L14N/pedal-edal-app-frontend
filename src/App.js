d69227331114a59320367b31b4f8ce47270499b1
RatBoyJim <>
2021-03-18 13:07:30 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import { NavBar, Footer, Loading } from "./components";
 import Header from './Components/Header.js'
+
 
 
 import './App.css';
@@ -10,7 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Header/>
-          
+        <NavBar/>
         
         <Switch>
         
---
+++ src/Components/main-nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import MainNav from "./main-nav";
+import AuthNav from "./auth-nav";
+
+const MainNav = () => {
+  return (
+    <div className="nav-container">
+      <h4>Main nav container</h4>
+    </div>
+  );
+};
+
+export default MainNav;
