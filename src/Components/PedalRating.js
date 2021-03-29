8296ec12c6eed9cb1900065110a6b14ee206b764
RatBoyJim <>
2021-03-29 10:32:34 +0100

    pedal ratings value set to db pedal rating

--- src/Components/PedalRating.js
+++ src/Components/PedalRating.js
@@ -1,9 +1,9 @@
 
 
-const PedalRating = () => {
+const PedalRating = (pedal) => {
 
     return(
-        <input type="range" min="1" max="5" id="rating-slider"/>
+        <input type="range" min="1" max="5" id="rating-slider" value={pedal.pedalRating} disabled/>
     )
 
 }
