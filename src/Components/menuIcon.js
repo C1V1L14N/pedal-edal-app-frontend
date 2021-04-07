5c034df3e89980fda6c7f84e8f9011b62c8091df
Kenny <kenny.mckeown@gmail.com>
2021-04-07 13:09:11 +0100

    step back a little

---
+++ src/Components/menuIcon.js
@@ -0,0 +1,11 @@
+import MenuImg from '../Assets/menu-icon.png';
+
+const MenuIcon = () => {
+
+    return (
+        <img id='menu' src={MenuImg} alt="user-menu"></img>
+    )
+
+}
+
+export default MenuIcon;
