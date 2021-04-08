47e1ab05d89dcb4a843594352bb9ac138e88d6c1
Kenny <kenny.mckeown@gmail.com>
2021-04-08 10:43:31 +0100

    done some stuff

---
+++ src/Components/pedalDetailsPage.js
@@ -0,0 +1,16 @@
+
+
+const PedalDetailsPage = () => {
+
+    return(
+        <div>
+            <h2>Pedal Name</h2>
+            <p>Description</p>
+            <label name="owned-pedal">Owned?</label>
+            <input type="checkbox" name="owned-pedal"/>
+        </div>
+    )
+
+}
+
+export default PedalDetailsPage;
