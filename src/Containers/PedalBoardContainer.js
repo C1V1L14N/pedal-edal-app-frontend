98b5224d72336123ca7d2b8ce43a20c652a75dae
RatBoyJim <>
2021-04-20 10:24:36 +0100

    working a wee bit

--- src/Containers/PedalBoardContainer.js
+++ src/Containers/PedalBoardContainer.js
@@ -7,22 +7,33 @@ import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 const PedalBoard = ({userPedals}) => {
 
     const [resized, setResized] = useState(false);
+    const [userPedalsState, setUserPedalsState] = useState(userPedals)
 
     const makeToScale = (pedal) => {
         pedal.width = Math.round((pedal.width * 3.7795275591)/3)
         pedal.length = Math.round((pedal.length * 3.7795275591)/3)
         }
 
+    function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(userPedalsState);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setUserPedalsState(items);
+  }
+
     
 
         return(
             <div>
                 <h2>Your Pedals:</h2>
-            <DragDropContext>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
                 <Droppable droppableId="droppable-pedal-list">
                     {(provided) => (
-                        <ul id="droppable-pedal-list" {...provided.droppableProps} ref={provided.innerRef}>
-                            {userPedals.map((pedal, index) => {
+                        <div id="droppable-pedal-list" {...provided.droppableProps} ref={provided.innerRef}>
+                            {userPedalsState.map((pedal, index) => {
                                 if(resized === false){
                                     console.log("width before", pedal.width);
                                     makeToScale(pedal)
@@ -32,19 +43,29 @@ const PedalBoard = ({userPedals}) => {
                                 return (
                                     <Draggable key={pedal.id} draggableId={pedal.name} index={index}>
                                         {(provided) => (
-                                                <li id="pedal-board-pedals-container" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                <div id="pedal-board-pedals-container" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                                                 <img src={process.env.PUBLIC_URL + pedal.image} height={pedal.length} width={pedal.width} />
-                                                </li>
+                                                </div>
                                             )}
                                     </Draggable>
                                 );
                             })}
                             {provided.placeholder}
-                        </ul>
+                        </div>
+                    )}
+                </Droppable>
+                </DragDropContext>
+                <DragDropContext>
+                <Droppable droppableId="pedal-board-itself">
+                    {(provided) => (
+                        <div id="pedal-board-itself" {...provided.droppableProps} ref={provided.innerRef}>
+                    </div>
                     )}
                 </Droppable>
             </DragDropContext>
+            
             </div>
+            
                     
         )
 
